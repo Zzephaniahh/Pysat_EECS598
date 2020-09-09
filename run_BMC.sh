0aafc4d49476f4b07094954e0e61224abd2e3d41
zzephaniahh <zeph@umich.edu>
2020-09-09 17:25:37 -0400

    rm some old files

--- run_BMC.sh
+++
@@ -1,8 +0,0 @@
-
-# the python file is name BMC and takes 3 arguments:
-# input_file
-# output_file
-# integer unrolling_depth
-python BMC.py s27.isc output_test.isc 3
-
-# After generating output_test.isc you can use the ISCAS_to_CNF.py then import to pysat
