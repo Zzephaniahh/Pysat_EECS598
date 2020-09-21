12831325f3dffbd255ae2747b3c5058e59785566
zzephaniahh <zeph@umich.edu>
2020-09-21 12:06:23 -0400

    rm some unneeded files

--- unroll.sh
+++
@@ -1,9 +0,0 @@
-INPUT_FILE=$1
-UNROLLING_DEPTH=$2
-OUTPUT_FILE=$3
-
-./unroll_isc $INPUT_FILE ${INPUT_FILE}_temp.delete $UNROLLING_DEPTH
-
-./iscas2symcnf ${INPUT_FILE}_temp.delete $OUTPUT_FILE
-
-rm *_temp.delete
